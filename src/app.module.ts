8c7f2992e863a26961cbb81cfc98be9f346da424
DanMainaChiwaz <daniel.maina@vivifysolutions.net>
2024-01-12 17:17:12 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { FacilityModule } from './facility/facility.module';
+import { EventModule } from './event/event.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, FacilityModule, EventModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
